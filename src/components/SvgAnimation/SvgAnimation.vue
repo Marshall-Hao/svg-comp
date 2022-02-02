43c6e0e6812ead9f03477123648b216e3a7187d1
Marshall Chan <marshallchan666@outlook.com>
2022-02-02 23:20:26 +0800

    SMIL, path, animate, animateTransform, animateMotion

--- src/components/SvgAnimation/SvgAnimation.vue
+++ src/components/SvgAnimation/SvgAnimation.vue
@@ -82,6 +82,189 @@
       ></path>
     </svg>
   </div>
+  <div class="container">
+    <svg width="200" height="200">
+      <rect x="0" y="0" fill="red" width="100" height="50">
+        <set attributeName="x" attributeType="XML" to="10" begin="1s"></set>
+        <set attributeName="x" attributeType="XML" to="20" begin="2s"></set>
+        <set
+          attributeName="fill"
+          attributeType="XML"
+          to="blue"
+          begin="3s"
+        ></set>
+      </rect>
+    </svg>
+  </div>
+  <div class="container">
+    <svg width="200" height="200">
+      <circle cx="0" cy="0" r="30" fill="blue">
+        <!-- * XML 代表是从dom里面的属性 -->
+        <!-- repeatCount="indefinite" -->
+        <!-- fill="remove" 回到原点 -->
+        <animate
+          attributeName="cx"
+          attributeType="XML"
+          from="0"
+          to="50"
+          dur="1s"
+          repeatCount="2"
+          fill="freeze"
+        ></animate>
+        <animate
+          attributeName="cy"
+          attributeType="XML"
+          from="0"
+          to="50"
+          dur="1s"
+          repeatCount="2"
+          fill="freeze"
+        ></animate>
+        <animate
+          attributeName="fill"
+          attributeType="XML"
+          from="blue"
+          to="red"
+          dur="1s"
+          repeatCount="2"
+          fill="freeze"
+        ></animate>
+        <animateTransform
+          attributeName="transform"
+          attributeType="XML"
+          type="scale"
+          from="1"
+          to="2"
+          dur="1s"
+          repeatCount="2"
+          fill="freeze"
+        ></animateTransform>
+      </circle>
+    </svg>
+  </div>
+  <div class="container">
+    <svg width="400" height="400">
+      <polygon points="30 30 70 30 90 70 10 70" fill="#fcc" stroke="black">
+        <animate
+          attributeName="points"
+          attributeType="XML"
+          to="50 30 70 50 50 90 30 50"
+          dur="5s"
+          fill="freeze"
+          repeatCount="1"
+        />
+      </polygon>
+    </svg>
+  </div>
+  <div class="container">
+    <svg width="200" height="200">
+      <rect x="0" y="0" fill="red" width="10" height="10">
+        <!-- * 核心功能 -->
+        <animateMotion
+          path="M10 10 L110 10 L110 110 L10 110 Z"
+          dur="5s"
+          rotate="0"
+          fill="freeze"
+          repeatCount="2"
+        ></animateMotion>
+      </rect>
+      <!-- d是路径 M起点 L中途的线 Z终点 -->
+      <path
+        d="M10 10 L110 10 L110 110 L10 110 Z"
+        fill="none"
+        stroke="green"
+      ></path>
+    </svg>
+  </div>
+  <div class="container">
+    <svg width="200" height="200">
+      <rect x="0" y="0" fill="red" width="10" height="10">
+        <!-- * 核心功能 -->
+        <animateMotion
+          id="forward-rect"
+          path="M10 10 L110 10 L110 110 L10 110"
+          dur="2s"
+          rotate="0"
+          fill="freeze"
+          begin="0; backward-rect.end + 0.5s"
+        ></animateMotion>
+        <!-- * begin="forward-rect.end" 动态获取上一个 动画结束时间-->
+        <animateMotion
+          id="backward-rect"
+          path="M10 110 L110 110 L110 10 L10 10"
+          dur="2s"
+          rotate="0"
+          fill="freeze"
+          begin="forward-rect.end + 0.5s"
+        ></animateMotion>
+        <animate
+          id="blue-to-red"
+          attributeName="fill"
+          attributeType="XML"
+          from="blue"
+          to="red"
+          dur="2s"
+          fill="freeze"
+          begin="0; red-to-blue.end + 0.5s"
+        ></animate>
+        <animate
+          id="red-to-blue"
+          attributeName="fill"
+          attributeType="XML"
+          from="red"
+          to="blue"
+          dur="2s"
+          fill="freeze"
+          begin="blue-to-red.end + 0.5s"
+        ></animate>
+      </rect>
+      <!-- d是路径 M起点 L中途的线 Z终点 -->
+      <path
+        d="M10 10 L110 10 L110 110 L10 110"
+        fill="none"
+        stroke="green"
+      ></path>
+    </svg>
+  </div>
+  <div class="container">
+    <svg viewBox="0 0 200 200" width="200" height="200">
+      <g id="rect1">
+        <rect x="0" y="0" rx="0" ry="0" width="100" height="100" fill="red">
+          <animate
+            attributeType="XML"
+            attributeName="fill"
+            from="red"
+            to="green"
+            begin="rect1.click"
+            dur="2s"
+            fill="freeze"
+          />
+        </rect>
+      </g>
+      <!-- * 作用域是在整个svg之下 所以整个svg位移 -->
+      <animateTransform
+        attributeType="XML"
+        attributeName="transform"
+        type="translate"
+        from="0, 0"
+        to="50, 50"
+        begin="rect1.click"
+        dur="2s"
+        fill="freeze"
+      />
+      <rect x="0" y="100" width="100" height="100" fill="blue">
+        <animate
+          attributeType="XML"
+          attributeName="fill"
+          from="blue"
+          to="green"
+          begin="rect1.click"
+          dur="2s"
+          fill="freeze"
+        />
+      </rect>
+    </svg>
+  </div>
 </template>
 
 <script>
